a0da5c6206fecf250d64f8828a9b41d113b7c4c7
Evan <evturn@gmail.com>
2014-12-14 13:51:22 -0500

    adding methods to collections

--- app/assets/javascripts/backbone/collections/contacts.js
+++ app/assets/javascripts/backbone/collections/contacts.js
@@ -1,7 +1,8 @@
 var ContactsCollection = Backbone.Collection.extend({
 	model: Contact,
 	maleContacts: function(){
-		this.models.filter(function(contact){
+		return this.models.filter(
+			function(contact){
 			return contact.get('gender') === "Male"
 		})
 	}
